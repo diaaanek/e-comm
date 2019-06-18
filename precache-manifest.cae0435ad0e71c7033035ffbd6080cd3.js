c164923cb766125b7d4888fd8c53ef83fe757dc7
diaaanek <dkorongy@gmail.com>
2019-06-17 21:51:32 -0400

    Updates

---
+++ precache-manifest.cae0435ad0e71c7033035ffbd6080cd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e7f44c07bcdf04f32db343037dbe62",
+    "url": "/ecomm/index.html"
+  },
+  {
+    "revision": "79f51b248fc8096359b4",
+    "url": "/ecomm/static/css/main.cd534fb8.chunk.css"
+  },
+  {
+    "revision": "9acc064082b09e720d7f",
+    "url": "/ecomm/static/js/2.e1e8cd57.chunk.js"
+  },
+  {
+    "revision": "79f51b248fc8096359b4",
+    "url": "/ecomm/static/js/main.261bb64a.chunk.js"
+  },
+  {
+    "revision": "7f85b29fc15e83ee35f5",
+    "url": "/ecomm/static/js/runtime~main.91579587.js"
+  }
+]);
